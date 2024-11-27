e1972ae6b6cc0b3131144e7747bf0dfd578d9fd7
sizem <muhammadxalilov726@gmail.com>
2024-11-27 19:19:30 +0500

    add:landing figma

---
+++ js/index.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        screens: {
+          mobile: "375px",
+          tablet: "780px",
+          desktop: "1280px",
+        },
+      },
+    },
+  },
+};
